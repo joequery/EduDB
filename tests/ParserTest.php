a58119bdbab483c6b6c0ed02c7f36ea01ea64ee2
Joseph McCullough <joseph@vertstudios.com>
2015-10-25 23:30:28 +0000

    namespace structure finally working. wow

--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -1,20 +1,11 @@
 <?php
-require('vendor/autoload.php');
-
 use EduDB\Parser;
-
 
 class ParserTest extends PHPUnit_Framework_TestCase
 {
     public function test_parse_columns(){
         $obj = new Parser();
-        //$this->assertEquals(1, $obj->parse());
-        /*
-        foreach(get_declared_classes() as $name){
-            echo $name . "\n";
-        }
-         */
-        $this->assertEquals(1,1);
+        $this->assertEquals(1, $obj->parse("h"));
     }
 }
 
