593de4ce2272fe7156bf950fcc6e44fd5a4e0483
Joseph McCullough <joseph@vertstudios.com>
2015-10-25 23:53:55 +0000

    ok, whatever

--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -1,11 +1,12 @@
 <?php
+namespace EduDB\Test;
 use EduDB\Parser;
 
-class ParserTest extends PHPUnit_Framework_TestCase
+class ParserTest extends \PHPUnit_Framework_TestCase
 {
     public function test_parse_columns(){
         $obj = new Parser();
-        $this->assertEquals(1, $obj->parse("h"));
+        $this->assertEquals(0, $obj->parse("h"));
     }
 }
 
